1d4a50931165b2a7f08fc69ab58996d1aefd073f
Jonathan Lucas de Matos Diniz <jonathan.lucas.m@gmail.com>
2023-07-04 14:41:02 -0300

    setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,21 @@
-echo "Installing asdf"
-yay -S asdf-vm
+echo "Configuring pacman"
+sudo cp pacman/pacman.conf /etc/pacman.conf
+sudo cp pacman/mirrorlist /etc/pacman.d/mirrorlist
+
+echo "Installing tools"
+yay -S aws-cli kubectl asdf-vm
 mkdir ~/.asdf
+
+#yay -S docker docker-compose
+
 echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "Installing p10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-sudo cp pacman/pacman.conf /etc/pacman.conf
-sudo cp pacman/mirrorlist /etc/pacman.d/mirrorlist
+echo "Installing lunar vim"
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
 
 cp .zshrc ~/
 source ~/.zshrc
