913d32b7cbe318250c42e5c86cc022a6b1154fbc
Jonathan Lucas de Matos Diniz <jonathan.lucas.m@gmail.com>
2023-07-04 12:38:42 -0300

    setup script

--- setup.sh
+++ setup.sh
@@ -7,5 +7,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo "Installing p10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+sudo cp pacman/pacman.conf /etc/pacman.conf
+sudo cp pacman/mirrorlist /etc/pacman.d/mirrorlist
+
 cp .zshrc ~/
 source ~/.zshrc
