9cda9bad1c605cfcd4cb5749d9c5bb1de88217b2
Jonathan Lucas de Matos Diniz <jonathan.lucas.m@gmail.com>
2023-07-04 12:21:55 -0300

    setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
+echo "Installing asdf"
 yay -S asdf-vm
 mkdir ~/.asdf
+echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Installing p10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 cp .zshrc ~/
 source ~/.zshrc
